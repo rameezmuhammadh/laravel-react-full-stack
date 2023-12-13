3000d6db978f2e7cd287697965403e604db549ab
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-13 14:29:28 +0530

    feat : created the login sign up pages

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,8 +1,23 @@
-export default function Login(){
+import { Link } from "react-router-dom";
 
-    return(
-        <div>
-            Login
+export default function Login() {
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+  };
+
+  return (
+    <div className="login-signup-form animated fadeInDown">
+    <div className="form">
+        <form onSubmit={onSubmit}>
+            <h1 className="title">Login in your account</h1>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button className="btn btn-block">Login</button>
+          <p className="message">
+            Not Registered? <Link to="/signup">Create an account</Link>
+          </p>
+        </form>
         </div>
-    )
+        </div>
+  );
 }
