b98c9dbacf4c79cd6ed25c30ba1f21ef0e7d2a75
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-13 22:42:29 +0530

    fix : coded for sign up not for login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,6 +12,10 @@ class AuthController extends Controller
 {
     public function login(LoginRequest $request){
         
+        
+    }
+    
+    public function signup(SignupRequest $request){
         $data = $request->validated();
         $user =User::create([
             'name' => $data['name'],
@@ -22,9 +26,7 @@ class AuthController extends Controller
       $token =  $user->createToken('main')->plainTextToken;
       return response(compact('user', 'token'));
     }
-    public function signup(SignupRequest $request){
-        
-    }
+
     public function logout(Request $request){
         
     }
