ea94ac25fd8c41f978349e35bae09f65d929e7fd
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-13 22:40:01 +0530

    feat: created the auth controller and coded the login function

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request){
+        
+        $data = $request->validated();
+        $user =User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+      $token =  $user->createToken('main')->plainTextToken;
+      return response(compact('user', 'token'));
+    }
+    public function signup(SignupRequest $request){
+        
+    }
+    public function logout(Request $request){
+        
+    }
+}
