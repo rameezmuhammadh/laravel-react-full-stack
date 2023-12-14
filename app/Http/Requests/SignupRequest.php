d1bde7530286aab180057570e0ae83d722bcd010
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-14 16:18:33 +0530

    fix: coded the signup request

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -3,15 +3,16 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
-class SignupRequest extends FormRequest
+class signupRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,15 @@ class SignupRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:55',
+            'email' => 'required|email|unique:users,email',
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)
+                    ->letters()
+                    ->symbols()
+            ]
         ];
     }
 }
