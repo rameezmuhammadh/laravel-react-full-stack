5ddade49f612d8413a4f136535d3cacace3c94e8
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-14 16:17:02 +0530

    fix : update the functions

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,32 +2,61 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
-use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\SignupRequest;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\signupRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request){
-        
-        
-    }
-    
-    public function signup(SignupRequest $request){
+    /**
+     * Display a signup.
+     */
+    public function signup(signupRequest $request)
+    {
         $data = $request->validated();
-        $user =User::create([
+
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
 
-      $token =  $user->createToken('main')->plainTextToken;
-      return response(compact('user', 'token'));
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact( 'user','token'));
     }
 
-    public function logout(Request $request){
-        
+    /**
+     * Display a login.
+     */
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Provided email address or password is incorrect'
+            ], 422);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact( 'user','token'));
     }
+
+    /**
+     * Display a logout.
+     */
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response('',204);
+    }
+
 }
